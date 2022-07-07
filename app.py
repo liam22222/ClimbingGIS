5d05c8403618bf230e3906fa13ac660ebba8ec8f
Sagi Sarussi <sagis@sagis.dev>
2022-07-07 22:25:50 +0300

    Fix login Reformat code

--- app.py
+++ app.py
@@ -1,35 +1,33 @@
-from flask import Flask, request, render_template
-from geojson import Point
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy  import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import json
-import email_validator
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+import pathlib
 
-engine = create_engine('sqlite:///database.db')
+DB_PATH = f'sqlite:///{pathlib.Path("database.db").resolve()}'
+engine = create_engine(DB_PATH)
 meta = MetaData()
 
 user_table = Table(
-   'user', meta,
-   Column('id', String),
-   Column('username', String, primary_key = True),
-   Column('email', String),
-   Column('password', String),
+    'user', meta,
+    Column('id', String),
+    Column('username', String, primary_key=True),
+    Column('email', String),
+    Column('password', String),
 )
 meta.create_all(engine)
-
-
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 my_path = 'linodeStreetTrees.geojson'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_PATH
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -42,14 +40,17 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
+
 
 class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
@@ -60,6 +61,7 @@ class RegisterForm(FlaskForm):
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -79,9 +81,9 @@ def login():
                 return redirect(url_for('dashboard'))
 
         return '<h1>Invalid username or password</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
 
     return render_template('login.html', form=form)
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -93,15 +95,16 @@ def signup():
         db.session.add(new_user)
         db.session.commit()
 
-        return redirect(url_for('/'))
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+        return redirect(url_for('maps'))
 
     return render_template('signup.html', form=form)
+
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
     return render_template('dashboard.html', name=current_user.username)
+
 
 @app.route('/logout')
 @login_required
@@ -117,7 +120,7 @@ def maps():
         print(street_trees_points)
         return render_template('template.html', street_trees_points=street_trees_points)
 
-if __name__ == '__main__':
-    app.debug=False
-    app.run(port=8099)
 
+if __name__ == '__main__':
+    app.debug = False
+    app.run(port=8099)
